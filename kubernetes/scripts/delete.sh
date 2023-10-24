0200f8f76f271bad96a3f365dd9992859ab2b87c
arnulfoacevedo <arnulfoacevedo215@gmail.com>
2023-10-23 21:14:08 -0500

    generator fix lib updates deployment updates

--- kubernetes/scripts/delete.sh
+++ kubernetes/scripts/delete.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
-kubectl delete -k "$(pwd)"/kubernetes/configs/cache \
-               -k "$(pwd)"/kubernetes/configs/db \
-               -k "$(pwd)"/kubernetes/configs/directus
+kubectl delete -k "$(pwd)"/kubernetes/configs/config-map
+kubectl delete -k "$(pwd)"/kubernetes/configs/cache
+kubectl delete -k "$(pwd)"/kubernetes/configs/db
+kubectl delete -k "$(pwd)"/kubernetes/configs/directus
--- kubernetes/scripts/deploy.sh
+++ kubernetes/scripts/deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-kubectl apply -k "$(pwd)"/kubernetes/configs/config-map \
-              -k "$(pwd)"/kubernetes/configs/cache \
-              -k "$(pwd)"/kubernetes/configs/db \
-              -k "$(pwd)"/kubernetes/configs/directus
+kubectl apply -k "$(pwd)"/kubernetes/configs/config-map
+kubectl apply -k "$(pwd)"/kubernetes/configs/cache
+kubectl apply -k "$(pwd)"/kubernetes/configs/db
+kubectl apply -k "$(pwd)"/kubernetes/configs/directus
